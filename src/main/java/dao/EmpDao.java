508308a97d403696ddb47aa9374127a83bb96c36
goodee <goodee@DESKTOP-0RMCPDQ>
2023-01-05 14:59:39 +0900

    2023-01-05 15:00 태환 menu

--- src/main/java/dao/EmpDao.java
+++ src/main/java/dao/EmpDao.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import vo.Customer;
 import vo.Emp;
 
 public class EmpDao {
@@ -25,21 +26,46 @@ public class EmpDao {
 	}
 	
 	// 2) emp 수정
-	public int updateEmp(Connection conn, Emp emp) {
-		return 0;
+	public int updateEmp(Connection conn, Emp emp) throws Exception {
+		int row = 0;
+		
+		String sql = "UPDATE SET emp"
+				+ " emp_name = ?"
+				+ " WHERE emp_id = ?";
+		
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setString(1, emp.getEmpId());
+		stmt.setString(2, emp.getEmpName());
+
+		row = stmt.executeUpdate(); // 1 반환 시, emp수정 성공
+		return row;
 		
 	}
+	
 	// 3) emp 비밀번호 수정
-	public int updateEmpPw(Connection conn, Emp emp, String newEmpPw) {
-		return 0;
+	public int updateEmpPw(Connection conn, Emp emp, String newCustomerPw) throws Exception {
+		int row = 0;
+		
+		String sql = "UPDATE SET emp"
+				+ " emp_pw = ?"
+				+ " WHERE emp_id = ? AND emp_pw = ?";
+		
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setString(1, newCustomerPw);
+		stmt.setString(2, emp.getEmpId());
+		stmt.setString(3, emp.getEmpPw());
+		
+		row = stmt.executeUpdate(); // 1 반환 시, 비밀번호 변경 성공
+		return row;
 		
 	}
+	
 	// 4) emp 활성화/비활성화
 	public int activeEmp() {
 		return 0;
 		
 	}
-	// 5) emp 레벨 변경
+	// 5) emp auth_code 변경
 	
 	// 6) emp 탈퇴
 	
@@ -47,7 +73,7 @@ public class EmpDao {
 	public Emp loginEmp(Connection conn, Emp emp) throws Exception {
 		Emp resultEmp = null;
 		
-		String sql = "SELECT emp_id, emp_name"
+		String sql = "SELECT emp_id, emp_name, auth_code, level"
 				+ " FROM emp"
 				+ " WHERE emp_id = ? AND emp_pw = ?";
 		
@@ -61,6 +87,8 @@ public class EmpDao {
 			resultEmp = new Emp();
 			resultEmp.setEmpId(rs.getString("emp_id"));
 			resultEmp.setEmpName(rs.getString("emp_name"));
+			resultEmp.setAuthCode(rs.getInt("auth_code"));
+			resultEmp.setLevel(rs.getInt("level"));
 		}
 		
 		return resultEmp; // 로그인 정보 반환
---
+++ src/main/webapp/inc/menuForEmp.jsp
@@ -0,0 +1,38 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<div>
+	<a href="${pageContext.request.contextPath}/Home">로고</a>
+	<a href="${pageContext.request.contextPath}/MyPage">마이페이지</a>
+	<a href="${pageContext.request.contextPath}/Logout">로그아웃</a>
+</div>
+
+<div>
+	<a href="${pageContext.request.contextPath}/Home">HOME</a>
+	
+	<c:if test="${loginMember.authCode == 1}">
+		<a href="${pageContext.request.contextPath}/">ADMIN</a>
+		<span>상품관리</span>
+	</c:if>
+	
+	<c:if test="${loginMember.authCode == 2}">
+		<a href="${pageContext.request.contextPath}/">ADMIN</a> <!-- 리스트 구현예정(상품관리) -->
+		<span>상품관리</span>
+		<span>주문관리</span>
+		<span>공지관리</span>
+	</c:if>
+	
+	<c:if test="${loginMember.authCode == 3}">
+		<a href="${pageContext.request.contextPath}/">ADMIN</a> <!-- 리스트 구현예정(상품관리) -->
+		<span>상품관리</span>
+		<span>주문관리</span>
+		<span>공지관리</span>
+		<span>사원관리</span>
+	</c:if>
+	
+	<a href="${pageContext.request.contextPath}/">CATEGORY</a> <!-- 리스트 구현예정 -->
+	<a href="${pageContext.request.contextPath}/Cart">CART</a>
+	<a href="${pageContext.request.contextPath}/ORDER">ORDER</a>
+	<a href="${pageContext.request.contextPath}/NOTICE">NOTICE</a>
+	<div>${loginMember.empName}님 환영합니다.</div>
+	<div>관리자 레벨 : ${loginMember.authCode}</div>
+</div>
